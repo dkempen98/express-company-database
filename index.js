f5587d35762d2b5dadb516754ce1c6d9e2e565e2
Drew <drewkempen98@gmail.com>
2022-06-09 23:45:13 -0400

    quick fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection(
     {
       host: 'localhost',
       user: 'root',
-      password: 'Monstars',
+      password: '',
       database: 'company_db'
     },
     console.log(`Connected to the classlist_db database.`)
